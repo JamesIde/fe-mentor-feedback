cba494c699fca224db700f5fd1f2f4fcc72fc6a4
James <james.ide775@gmail.com>
2023-02-12 12:37:10 +1030

    Added lambda deployment github workflow

--- api/lambdas/feedback.ts
+++ api/lambdas/feedback.ts
@@ -1,8 +1,15 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { AccessService } from "../services/access.service";
-export const hello = async (
+export const getFeedback = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
   const accessService = new AccessService();
   return accessService.getFeedback();
+};
+
+export const addFeedback = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  const accessService = new AccessService();
+  return accessService.addFeedback(event);
 };
--- api/services/access.service.ts
+++ api/services/access.service.ts
@@ -1,16 +1,24 @@
+import { SupabaseClient } from "@supabase/supabase-js";
+import { APIGatewayProxyEvent } from "aws-lambda";
 import { supabase } from "../config/db";
 import response from "../utils/response";
 
 export class AccessService {
-  supabase: any;
+  supabase: SupabaseClient;
   constructor() {
     this.supabase = supabase;
   }
-  async getFeedback() {
+  public async getFeedback() {
     let { data: feedback, error } = await supabase.from("feedback").select("*");
     if (error) {
       return response(500, error.message);
     }
     return response(200, feedback);
+  }
+
+  public async addFeedback(event: APIGatewayProxyEvent) {
+    const body = JSON.parse(event.body!);
+
+    return response(201, body as any);
   }
 }
